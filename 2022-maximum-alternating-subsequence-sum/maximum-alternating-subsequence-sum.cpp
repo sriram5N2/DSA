af7d95162c933d19f6f455b5e474f73f6091bc53
sriram5N2 <139240678+sriram5N2@users.noreply.github.com>
2026-02-05 09:11:57 +0530

    Time: 282 ms (24.07%) | Memory: 156.8 MB (50.55%) - LeetSync

---
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        int N=nums.size();
+
+        vector<vector<long long>> dp(N,vector<long long>(2));
+        dp[0][0]=nums[0];
+        dp[0][1]=0;
+        for(int i=1;i<N;i++)
+        {
+            dp[i][0]=max(dp[i-1][0],dp[i-1][1]+(1LL*nums[i]));
+            dp[i][1]=max(dp[i-1][1],dp[i-1][0]-nums[i]);
+
+        }
+        return max(dp[N-1][0],dp[N-1][1]);
+        
+    }
+};

64400feb09956f0605bc8402fc4b1ed9b9c7be7f
sriram5N2 <139240678+sriram5N2@users.noreply.github.com>
2026-03-02 10:25:41 +0530

    Time: 514 ms (14.29%) | Memory: 382 MB (14.29%) - LeetSync

--- 4161-minimum-bitwise-or-from-grid/minimum-bitwise-or-from-grid.cpp
+++ 4161-minimum-bitwise-or-from-grid/minimum-bitwise-or-from-grid.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
-unordered_map<int,unordered_map<int,int>> dp;
-int global=INT_MAX;
-    int solve(int i,vector<vector<int>> &grid,int curr_or)
-    {
-        if(curr_or>=global) return 1e9;
-        if(i>=grid.size())
-        {
-               global=min(global,curr_or);
-               return curr_or;
-        }
-        if(dp[i].count(curr_or)) return dp[i][curr_or];
-       int ans=1e9;
-        for(int col=0;col<grid[0].size();col++)
-            {
-                int new_or=curr_or;
-                
-                    new_or=new_or|grid[i][col];
-               ans=min(ans,solve(i+1,grid,new_or));
-                    
-                
-            }
-        return dp[i][curr_or]=ans;
-    }
     int minimumOR(vector<vector<int>>& grid) {
-      
-        return solve(0,grid,0);
+
+        int ans=0;
+        int n=grid.size();
+        for(int bit=17;bit>=0;bit--)
+        {
+            vector<vector<int>> temp(n);
+            bool flag=true;
+            for(int i=0;i<grid.size();i++)
+            {
+                for(auto val:grid[i])
+                {
+                    if((val&(1<<bit))==0)
+                    {
+                        temp[i].push_back(val);
+                    }
+                }
+                if(temp[i].empty())
+                {
+                    ans=ans|(1<<bit);
+                    flag=false;
+                    break;
+                }
+
+            }
+            if(flag)
+            {
+                grid=temp;
+
+            }
+        }
+        return ans;
         
     }
 };
